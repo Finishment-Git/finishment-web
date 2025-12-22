7f6b521bb9da88f0ac54734d79249d3d63f07805
Finishment Admin <finishment@proton.me>
2025-12-22 11:13:01 -0600

    Final: Set new design as homepage by renaming page2 to page

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,17 @@
-import Navbar from "@/components/navbar";
-import Hero from "@/components/hero";
-import HowItWorks from "@/components/howitworks";
-import WhyUs from "@/components/whyus";
-import ContactUs from "@/components/contactus";
+import Navbar from "../components/navbar";
+import Hero from "../components/hero";
+import HowItWorks from "../components/howitworks";
+import WhyUs from "../components/whyus";
+import ContactUs from "../components/contactus";
 
-export default function Home() {
+export default function Page() {
   return (
-    <main className="min-h-screen flex flex-col bg-white text-black">
+    <main className="min-h-screen flex flex-col">
       <Navbar />
       <Hero />
       <HowItWorks />
       <WhyUs />
       <ContactUs />
-      
-      <footer className="py-10 border-t border-gray-100 text-center text-gray-500 text-sm mt-auto">
-        <p>&copy; 2024 Finishment Clone. Built with Next.js.</p>
-      </footer>
     </main>
   );
 }
