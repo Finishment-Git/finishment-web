14196e55c2e8bcf68ec3117c764b35b21cec9be5
Finishment Admin <finishment@proton.me>
2025-12-22 10:51:00 -0600

    Fix: Matched import casing to filenames

--- app/page2.tsx
+++ app/page2.tsx
@@ -1,8 +1,8 @@
-import Navbar from "../components/Navbar";
-import Hero from "../components/Hero";
-import HowItWorks from "../components/HowItWorks";
-import WhyUs from "../components/WhyUs";
-import ContactUs from "../components/ContactUs";
+import Navbar from "../components/navbar";
+import Hero from "../components/hero";
+import HowItWorks from "../components/howitworks";
+import WhyUs from "../components/whyus";
+import ContactUs from "../components/contactus";
 
 export default function Page() {
   return (
